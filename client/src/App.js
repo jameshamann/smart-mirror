2e691d9f3092d6d0a552bca5887d9cd89dff5ccc
James Hamann <jameshamann@me.com>
2018-02-20 18:42:09 +0000

    sets up react back with front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,23 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Home from './screens/home.js'
 
 class App extends Component {
-  state = {
-    response: ''
-  };
+  state = {users: []}
 
- componentDidMount() {
-   this.callApi()
-     .then(res => this.setState({ response: res.express }))
-     .catch(err => console.log(err));
- }
-
- callApi = async () => {
-   const response = await fetch('/api/hello');
-   const body = await response.json();
-
-   if (response.status !== 200) throw Error(body.message);
-
-   return body;
- };
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
 
   render() {
     return (
       <div className="App">
-        <Home />
-         <p className="App-intro" style={{color: 'white'}}>{this.state.response}</p>
+        <h1>Users</h1>
+        {this.state.users.map(user =>
+          <div key={user.id}>{user.username}</div>
+        )}
       </div>
     );
   }
--- client/src/components/calendar.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react'
-
-
-class Calendar extends Component {
-
-}
-
-export default Calendar;
