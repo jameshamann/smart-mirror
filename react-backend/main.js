158521347d153000064029f82718ca0a9f1d9228
James Hamann <jameshamann@me.com>
2018-03-09 19:37:07 +0000

    now runs as electron app

---
+++ react-backend/main.js
@@ -0,0 +1,18 @@
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 700
+    });
+
+    mainWindow.setTitle('Miri');
+    mainWindow.loadURL('http://localhost:3001');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
