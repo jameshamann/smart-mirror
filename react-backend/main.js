dd9021bdb051115fc0ff3ae514cdb0b5a1f2dfca
James Hamann <jameshamann@me.com>
2018-04-03 21:32:34 +0100

    build for prod and changing window size of electron app

--- react-backend/main.js
+++ react-backend/main.js
@@ -4,7 +4,7 @@ const { app, BrowserWindow } = electron;
 let mainWindow;
 
 app.on('ready', () => {
-        mainWindow = new BrowserWindow({width: 800, height: 600, x: 0, y: 0, kiosk:true, darkTheme: true, webPreferences: {nodeIntegration: false}})
+        mainWindow = new BrowserWindow({width: 300, height: 800, x: 0, y: 0, kiosk:true, darkTheme: true, webPreferences: {nodeIntegration: false}})
 
     mainWindow.setTitle('Miri');
     mainWindow.loadURL('http://localhost:3001');
