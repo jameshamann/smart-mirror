7ffc506b342defb7bb64d5735d92a2769781ce28
James Hamann <jameshamann@me.com>
2018-04-09 19:56:00 +0100

    inits project with awsamplify

--- react-backend/main.js
+++ react-backend/main.js
@@ -7,7 +7,7 @@ app.on('ready', () => {
         mainWindow = new BrowserWindow({width: 300, height: 800, x: 0, y: 0, kiosk:true, darkTheme: true, webPreferences: {nodeIntegration: true}})
 
     mainWindow.setTitle('Miri');
-    mainWindow.loadURL('http://localhost:3001');
+    mainWindow.loadURL('http://localhost:3000');
     mainWindow.setBackgroundColor("#000")
 
     mainWindow.on('closed', () => {
