a21c550356a7301e7ebd14e35ba42457bae87ba8
James Hamann <jameshamann@me.com>
2018-04-26 20:40:56 +0100

    connecting to iot

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './screens/home.js'
+import { PubSub } from 'aws-amplify';
+import { AWSIoTProvider } from 'aws-amplify/lib/PubSub/Providers';
+
 
 
 
